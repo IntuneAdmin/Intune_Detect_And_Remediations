8f7335d3882a9db55d99fa19d148729da026186b
IntuneAdmin <93092915+IntuneAdmin@users.noreply.github.com>
2023-10-07 11:56:41 +0200

    Add files via upload

---
+++ Disk_Events_Detect.ps1
@@ -0,0 +1,35 @@
+﻿# Define the log name and event IDs for disk errors and warnings
+$eventLogName = 'System'
+$errorEventID = 7     # The event ID for disk errors
+$warningEventID = 52  # The event ID for disk warnings
+
+# Get the latest disk error and warning events from the specified log
+$latestErrorEvent = Get-WinEvent -LogName $eventLogName -MaxEvents 1 | Where-Object { $_.Id -eq $errorEventID }
+$latestWarningEvent = Get-WinEvent -LogName $eventLogName -MaxEvents 1 | Where-Object { $_.Id -eq $warningEventID }
+
+# Check if there is a recent disk error or warning event
+if ($latestErrorEvent -or $latestWarningEvent) {
+    # There is a recent disk error or warning event
+    Write-Host "Disk issues detected:"
+    
+    if ($latestErrorEvent) {
+        Write-Host "Latest Disk Error Event:"
+        $latestErrorEvent | Format-Table -AutoSize
+    }
+    
+    if ($latestWarningEvent) {
+        Write-Host "Latest Disk Warning Event:"
+        $latestWarningEvent | Format-Table -AutoSize
+    }
+    
+    # Return a custom exit code to indicate an issue (e.g., 1)
+    exit 1
+    
+} else {
+    # No recent disk error or warning event
+    Write-Host "No recent disk issues detected."
+    
+    # Return a success exit code to indicate no issues (e.g., 0)
+    exit 0
+    
+}
---
+++ Disk_Events_Remediation.ps1
@@ -0,0 +1,31 @@
+﻿Install-Module -Name BurntToast -Force
+function Show-Notification {
+    param (
+        [string]$EventMessage
+    )
+
+   # Import the BurntToast module
+   Import-Module BurntToast
+
+    # Display a notification
+    New-BurntToastNotification -AppLogo 'C:\Path\To\Your\Logo.png' -Text $EventMessage
+}
+
+
+# Define the log name and event IDs for disk errors and warnings
+$eventLogName = 'System'
+$errorEventID = 7     # The event ID for disk errors
+$warningEventID = 52  # The event ID for disk warnings
+
+# Check if there is a recent disk error or warning event
+$latestErrorEvent = Get-WinEvent -LogName $eventLogName -MaxEvents 1 | Where-Object { $_.Id -eq $errorEventID }
+$latestWarningEvent = Get-WinEvent -LogName $eventLogName -MaxEvents 1 | Where-Object { $_.Id -eq $warningEventID }
+
+if ($latestErrorEvent -or $latestWarningEvent) {
+    # There is a recent disk error or warning event
+    $eventMessage = "Disk issues have been found!" + [Environment]::NewLine + "Contact IT support on 0118 999 881 999 119 725 3"
+    
+       
+    # Display the graphical notification
+    Show-Notification -EventMessage $eventMessage
+}
